0e74b45d8ced24ec314ff3bc3a8ac6aee757325c
Guttata <lefrancclement@gmail.com>
2018-02-15 21:18:53 +0100

    AppBar modification

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 /**
  * A simple example of `AppBar` with an icon on the right.
  * By default, the left icon is a navigation-menu.
  */
-const AppBarExampleIcon = () => (
+const AppBarHeader = () => (
+ <MuiThemeProvider>
   <AppBar
     title="Wavestone Autonomous Car"
     iconClassNameRight="muidocs-icon-navigation-expand-more"
   />
+  </MuiThemeProvider>
 );
 
-export default AppBar;
+export default AppBarHeader;
