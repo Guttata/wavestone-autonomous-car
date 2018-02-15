e30301684a25122e444f7b027d689665dc783d51
Guttata <lefrancclement@gmail.com>
2018-02-15 20:19:01 +0100

    add AppBar.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppBar from './AppBar';
 import registerServiceWorker from './registerServiceWorker';
 
+ReactDOM.render(<AppBar />, document.getElementById('AppBar'));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
